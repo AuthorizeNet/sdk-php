5e36a8be0da809b5eaa621e4b946b163e2650aa3
ashtru <ashu140510@gmail.com>
2015-10-16 17:49:53 +0530

    fixed log levels heirarchy

--- lib/net/authorize/util/LogHelper.php
+++ lib/net/authorize/util/LogHelper.php
@@ -3,18 +3,25 @@ namespace net\authorize\util;
 
 define ("ANET_LOG_FILES_APPEND",true);
 define ("ANET_LOG_FILE","phplog");
+
 define("ANET_LOG_DEBUG_PREFIX","DEBUG");
 define("ANET_LOG_INFO_PREFIX","INFO");
+define("ANET_LOG_WARN_PREFIX","WARN");
 define("ANET_LOG_ERROR_PREFIX","ERROR");
 
-define('ANET_LOG_DEBUG',false);
-define("ANET_LOG_INFO",true);
-define("ANET_LOG_ERROR",true);
+//log levels
+define('ANET_LOG_DEBUG',1);
+define("ANET_LOG_INFO",2);
+define("ANET_LOG_WARN",3);
+define("ANET_LOG_ERROR",4);
+//set level
+define("ANET_LOG_LEVEL",ANET_LOG_DEBUG);
+
 
 class Log
 {
     private function maskSensitiveString($rawString){
-        $this->debug("\n Start masking \n");
+//        echo("\n Start masking \n");
         //Tag name is compulsory, can leave patterns and repalcements blank
         $tags= array("cardCode","cardNumber","expirationDate");
         $patterns=array("","([^0-9]*)(\d+)(\d{4})(.*)","");
@@ -26,65 +33,58 @@ class Log
                 $inputPattern = "(.+)"; //no need to mask null data
             }
             $pattern = "/<" . $tag . ">". $inputPattern ."<\/" . $tag . ">/";
-            $this->debug("\npattern used: " . $pattern);
+//            echo("\npattern used: " . $pattern);
 
             $inputReplacement = "xxxx";
             if(trim($replacements[$i])) {
                 $inputReplacement = $replacements[$i];
             }
             $replacement = "<" . $tag . ">" . $inputReplacement . "</" . $tag . ">";
-            $this->debug("\nreplace string:" . $replacement);
-
+//            echo("\nreplace string:" . $replacement);
+//
             $patterns[$i]=$pattern;
             $replacements[$i]=$replacement;
         }
         $maskedString = preg_replace($patterns, $replacements, $rawString);
-        $this->debug( "\nreplaced: " . $maskedString . "\n\n");
+//        echo( "\nreplaced: " . $maskedString . "\n\n");
         return $maskedString;
     }
     private function getMasked($raw)
     {
+//        echo "raw data to mask:" . $raw;
         $messageType = gettype($raw);
-        $this->debug("Mesage Raw Type: ". $messageType);
+//        echo("Mesage Raw Type: ". $messageType);
         $message="";
         if ($messageType == "string") {
             $message = $this->maskSensitiveString($raw);
         }
+//        echo"\n here\n";
         return $message;
     }
-    public function debug($logMessage)
+    public function debug($logMessage, $flags=FILE_APPEND)
     {
-        if(ANET_LOG_DEBUG==1){
-            echo ANET_LOG_DEBUG_PREFIX . ": " //. \net\authorize\util\Helpers::now(). ":"
-                . "$logMessage" . "\n"; //print to console
+        if(ANET_LOG_DEBUG >= ANET_LOG_LEVEL){
+            $this->log(ANET_LOG_DEBUG_PREFIX, $logMessage,$flags);
         }
     }
     public function info($logMessage, $flags=FILE_APPEND){
-//        echo  ANET_LOG_INFO_PREFIX . ": " . \net\authorize\util\Helpers::now() . ":" . "$logMessage" . "\n"; //print to console
-
-        if(ANET_LOG_INFO==1) {
-            //masking
-            $logMessage = $this->getMasked($logMessage);
-            //time reference
-            $logString = ANET_LOG_INFO_PREFIX . ": " . \net\authorize\util\Helpers::now() . ":" . "$logMessage" . "\n"; //print to console
-
-            file_put_contents(ANET_LOG_FILE, "$logString", $flags);
-            echo $logString; //REMOVE
+        if(ANET_LOG_INFO >= ANET_LOG_LEVEL) {
+            $this->log(ANET_LOG_INFO_PREFIX, $logMessage,$flags);
         }
     }
     public function error($logMessage, $flags=FILE_APPEND){
-        if(ANET_LOG_ERROR==1) {
-            //masking
-            $logMessage = $this->getMasked($logMessage);
-            //time reference
-            $logString = ANET_LOG_ERROR_PREFIX . ": " . \net\authorize\util\Helpers::now() . ":" . "$logMessage" . "\n"; //print to console
-
-            file_put_contents(ANET_LOG_FILE, "$logString", $flags);
-            echo $logString; //REMOVE
+        if(ANET_LOG_ERROR >= ANET_LOG_LEVEL) {
+            $this->log(ANET_LOG_ERROR_PREFIX, $logMessage,$flags);
         }
     }
-
-
+    private function log($prefix, $logMessage, $flags){
+        //masking
+        $logMessage = $this->getMasked($logMessage);
+        //log level, timestamp
+        $logString = "\n".$prefix . ": " . \net\authorize\util\Helpers::now() . ":" . $logMessage;
+        file_put_contents(ANET_LOG_FILE, "$logString", $flags);
+        echo $logString; //REMOVE
+    }
 }
 
 class LogFactory
