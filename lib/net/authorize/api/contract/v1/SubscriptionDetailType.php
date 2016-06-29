a3e52a465e7c7501710d4b566e010cf29a746b65
140b8f67-ec51-4b64-9606-bff2dffa0170 <flyandi@yahoo.com>
2016-06-28 22:09:18 -0700

    Implementing TypeHelper in SubscriptionDetailType

--- lib/net/authorize/api/contract/v1/SubscriptionDetailType.php
+++ lib/net/authorize/api/contract/v1/SubscriptionDetailType.php
@@ -2,13 +2,15 @@
 
 namespace net\authorize\api\contract\v1;
 
+use \net\authorize\api\contract\v1\Extensions\TypeHelper;
+
 /**
  * Class representing SubscriptionDetailType
  *
  *
  * XSD Type: SubscriptionDetail
  */
-class SubscriptionDetailType
+class SubscriptionDetailType extends TypeHelper
 {
 
     /**
