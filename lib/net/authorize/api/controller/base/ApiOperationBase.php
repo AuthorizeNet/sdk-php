f822fabff2ed35a195f632e82ed18755d20e2d11
Ashish Kankaria <akankari@visa.com>
2016-08-12 18:00:00 +0530

    Update ApiOperationBase.php

--- lib/net/authorize/api/controller/base/ApiOperationBase.php
+++ lib/net/authorize/api/controller/base/ApiOperationBase.php
@@ -8,7 +8,7 @@ use GoetasWebservices\Xsd\XsdToPhpRuntime\Jms\Handler\BaseTypesHandler;
 use GoetasWebservices\Xsd\XsdToPhpRuntime\Jms\Handler\XmlSchemaDateHandler;
 
 use \net\authorize\util\HttpClient;
-use \net\authorize\util\Helpers;
+use \net\authorize\util\Helpers;p
 use \net\authorize\util\LogFactory as LogFactory;
 
 
@@ -114,6 +114,8 @@ abstract class ApiOperationBase implements IApiOperation
         $this->logger->info("Request Serialization Begin");
         $this->logger->debug($this->apiRequest);
         $xmlRequest = $this->serializer->serialize($this->apiRequest, 'xml');
+	
+	echo $xmlRequest;
 	
         $this->logger->info("Request  Serialization End");
         /*
