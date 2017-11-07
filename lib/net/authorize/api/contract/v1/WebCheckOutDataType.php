c2e6ad2b581825173d13c2a8e1ffe7b97dffc159
akale <akale@visa.com>
2017-11-07 15:19:00 +0530

    BIN CIM Support New files

---
+++ lib/net/authorize/api/contract/v1/WebCheckOutDataType.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace net\authorize\api\contract\v1;
+
+/**
+ * Class representing WebCheckOutDataType
+ *
+ * 
+ * XSD Type: webCheckOutDataType
+ */
+class WebCheckOutDataType
+{
+
+    /**
+     * @property string $type
+     */
+    private $type = null;
+
+    /**
+     * @property string $id
+     */
+    private $id = null;
+
+    /**
+     * @property \net\authorize\api\contract\v1\WebCheckOutDataType\TokenAType $token
+     */
+    private $token = null;
+
+    /**
+     * Gets as type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Sets a new type
+     *
+     * @param string $type
+     * @return self
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * Gets as id
+     *
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets a new id
+     *
+     * @param string $id
+     * @return self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * Gets as token
+     *
+     * @return \net\authorize\api\contract\v1\WebCheckOutDataType\TokenAType
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Sets a new token
+     *
+     * @param \net\authorize\api\contract\v1\WebCheckOutDataType\TokenAType $token
+     * @return self
+     */
+    public function setToken(\net\authorize\api\contract\v1\WebCheckOutDataType\TokenAType $token)
+    {
+        $this->token = $token;
+        return $this;
+    }
+
+
+}
+
---
+++ lib/net/authorize/api/contract/v1/WebCheckOutDataType/TokenAType.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace net\authorize\api\contract\v1\WebCheckOutDataType;
+
+/**
+ * Class representing TokenAType
+ */
+class TokenAType
+{
+
+    /**
+     * @property string $cardNumber
+     */
+    private $cardNumber = null;
+
+    /**
+     * @property string $expirationDate
+     */
+    private $expirationDate = null;
+
+    /**
+     * @property string $cardCode
+     */
+    private $cardCode = null;
+
+    /**
+     * @property string $zip
+     */
+    private $zip = null;
+
+    /**
+     * @property string $fullName
+     */
+    private $fullName = null;
+
+    /**
+     * Gets as cardNumber
+     *
+     * @return string
+     */
+    public function getCardNumber()
+    {
+        return $this->cardNumber;
+    }
+
+    /**
+     * Sets a new cardNumber
+     *
+     * @param string $cardNumber
+     * @return self
+     */
+    public function setCardNumber($cardNumber)
+    {
+        $this->cardNumber = $cardNumber;
+        return $this;
+    }
+
+    /**
+     * Gets as expirationDate
+     *
+     * @return string
+     */
+    public function getExpirationDate()
+    {
+        return $this->expirationDate;
+    }
+
+    /**
+     * Sets a new expirationDate
+     *
+     * @param string $expirationDate
+     * @return self
+     */
+    public function setExpirationDate($expirationDate)
+    {
+        $this->expirationDate = $expirationDate;
+        return $this;
+    }
+
+    /**
+     * Gets as cardCode
+     *
+     * @return string
+     */
+    public function getCardCode()
+    {
+        return $this->cardCode;
+    }
+
+    /**
+     * Sets a new cardCode
+     *
+     * @param string $cardCode
+     * @return self
+     */
+    public function setCardCode($cardCode)
+    {
+        $this->cardCode = $cardCode;
+        return $this;
+    }
+
+    /**
+     * Gets as zip
+     *
+     * @return string
+     */
+    public function getZip()
+    {
+        return $this->zip;
+    }
+
+    /**
+     * Sets a new zip
+     *
+     * @param string $zip
+     * @return self
+     */
+    public function setZip($zip)
+    {
+        $this->zip = $zip;
+        return $this;
+    }
+
+    /**
+     * Gets as fullName
+     *
+     * @return string
+     */
+    public function getFullName()
+    {
+        return $this->fullName;
+    }
+
+    /**
+     * Sets a new fullName
+     *
+     * @param string $fullName
+     * @return self
+     */
+    public function setFullName($fullName)
+    {
+        $this->fullName = $fullName;
+        return $this;
+    }
+
+
+}
+
