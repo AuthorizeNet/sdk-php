308446fbf25acff15ccf50b57969adf7a2ed9a01
Travis Fisher <travis@pneudart.com>
2017-11-29 10:31:39 -0500

    no message

--- lib/net/authorize/api/contract/v1/CustomerProfileMaskedType.php
+++ lib/net/authorize/api/contract/v1/CustomerProfileMaskedType.php
@@ -12,26 +12,6 @@ class CustomerProfileMaskedType extends CustomerProfileExType
 {
 
     /**
-     * @property string $customerProfileId
-     */
-    private $customerProfileId = null;
-    
-    /**
-     * @property string $merchantCustomerId
-     */
-    private $merchantCustomerId = null;
-
-    /**
-     * @property string $description
-     */
-    private $description = null;
-
-    /**
-     * @property string $email
-     */
-    private $email = null;
-
-    /**
      * @property \net\authorize\api\contract\v1\CustomerPaymentProfileMaskedType[]
      * $paymentProfiles
      */
@@ -41,94 +21,6 @@ class CustomerProfileMaskedType extends CustomerProfileExType
      * @property \net\authorize\api\contract\v1\CustomerAddressExType[] $shipToList
      */
     private $shipToList = null;
-
-    /**
-     * Gets as customerProfileId
-     *
-     * @return string
-     */
-    public function getCustomerProfileId()
-    {
-        return $this->customerProfileId;
-    }
-
-    /**
-     * Sets a new customerProfileId
-     *
-     * @param string $customerProfileId
-     * @return self
-     */
-    public function setCustomerProfileId($customerProfileId)
-    {
-        $this->customerProfileId = $customerProfileId;
-        return $this;
-    }
-
-    /**
-     * Gets as merchantCustomerId
-     *
-     * @return string
-     */
-    public function getMerchantCustomerId()
-    {
-        return $this->merchantCustomerId;
-    }
-
-    /**
-     * Sets a new merchantCustomerId
-     *
-     * @param string $merchantCustomerId
-     * @return self
-     */
-    public function setMerchantCustomerId($merchantCustomerId)
-    {
-        $this->merchantCustomerId = $merchantCustomerId;
-        return $this;
-    }
-
-    /**
-     * Gets as description
-     *
-     * @return string
-     */
-    public function getDescription()
-    {
-        return $this->description;
-    }
-
-    /**
-     * Sets a new description
-     *
-     * @param string $description
-     * @return self
-     */
-    public function setDescription($description)
-    {
-        $this->description = $description;
-        return $this;
-    }    
-
-    /**
-     * Gets as email
-     *
-     * @return string
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Sets a new email
-     *
-     * @param string $email
-     * @return self
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-        return $this;
-    }    
 
     /**
      * Adds as paymentProfiles
