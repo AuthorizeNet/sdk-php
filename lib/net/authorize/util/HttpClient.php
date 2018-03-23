468d37c0d923b225e940c85594f1c6704d5e3d44
Cody Wilson <cody.wilson@texvisions.com>
2018-03-23 10:45:22 -0400

    Add the ability to configure the CA PEM file location

--- lib/net/authorize/util/HttpClient.php
+++ lib/net/authorize/util/HttpClient.php
@@ -77,7 +77,11 @@ class HttpClient
         $this->logger->info(sprintf("Request to AnetApi: \n%s", $xmlRequest));
 
         if ($this->VERIFY_PEER) {
-            curl_setopt($curl_request, CURLOPT_CAINFO, dirname(dirname(__FILE__)) . '/../../ssl/cert.pem');
+            if (defined('AUTHORIZENET_CA_PEM_FILE')) {
+                curl_setopt($curl_request, CURLOPT_CAINFO, AUTHORIZENET_CA_PEM_FILE);
+            } else {
+                curl_setopt($curl_request, CURLOPT_CAINFO, dirname(dirname(__FILE__)) . '/../../ssl/cert.pem');
+            }
         } else {
             $this->logger->error("Invalid SSL option for the request");
             return false;
