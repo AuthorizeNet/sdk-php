e765077e2afbf8b85d0c0ab6ce2a9905ed3ffe38
ashtru <ashutosh41093@gmail.com>
2018-07-24 16:22:24 +0530

    Modified deprecation message with more details for new features

--- lib/deprecated/AuthorizeNetAIM.php
+++ lib/deprecated/AuthorizeNetAIM.php
@@ -1,9 +1,11 @@
 <?php
 /**
-* @deprecated since version 1.9.8
-* @deprecated The Authorize.Net API has been reorganized to simplify & ease integration and to be more merchant focused
-* @deprecated AIM, ARB, CIM, Reporting and SIM have all been deprecated in favor of AuthorizeNet::API.
-* @deprecated For AIM, refer examples in https://github.com/AuthorizeNet/sample-code-php/tree/master/PaymentTransactions
+ * @deprecated since version 1.9.8
+ * @deprecated We have reorganized and simplified the Authorize.Net API to ease integration and to focus on merchants' needs.
+ * @deprecated We have deprecated AIM, ARB, CIM, and Reporting as separate options, in favor of AuthorizeNet::API.
+ * @deprecated We have also deprecated SIM as a separate option, in favor of Accept Hosted. See https://developer.authorize.net/api/reference/features/accept_hosted.html for details on Accept Hosted.
+ * @deprecated For details on the deprecation and replacement of legacy Authorize.Net methods, visit https://developer.authorize.net/api/upgrade_guide/. 
+ * @deprecated For AIM, refer examples in https://github.com/AuthorizeNet/sample-code-php/tree/master/PaymentTransactions
 */
 trigger_error('AuthorizeNetAIM is deprecated, use AuthorizeNet::API instead. For AIM, see examples in https://github.com/AuthorizeNet/sample-code-php/tree/master/PaymentTransactions .', E_USER_DEPRECATED);
 
--- lib/deprecated/AuthorizeNetCP.php
+++ lib/deprecated/AuthorizeNetCP.php
@@ -1,9 +1,12 @@
 <?php
 /**
-* @deprecated since version 1.9.8
-* @deprecated The Authorize.Net API has been reorganized to simplify & ease integration and to be more merchant focused
-* @deprecated AIM, ARB, CIM, Reporting and SIM have all been deprecated in favor of AuthorizeNet::API.
-* @deprecated CP and CNP both use similar request structure (with differences in payment fields). For CP, refer examples in https://github.com/AuthorizeNet/sample-code-php/tree/master/PaymentTransactions
+ * @deprecated since version 1.9.8
+ * @deprecated We have reorganized and simplified the Authorize.Net API to ease integration and to focus on merchants' needs.
+ * @deprecated We have deprecated AIM, ARB, CIM, and Reporting as separate options, in favor of AuthorizeNet::API.
+ * @deprecated We have also deprecated SIM as a separate option, in favor of Accept Hosted. See https://developer.authorize.net/api/reference/features/accept_hosted.html for details on Accept Hosted.
+ * @deprecated For details on the deprecation and replacement of legacy Authorize.Net methods, visit https://developer.authorize.net/api/upgrade_guide/. 
+ * @deprecated CP and CNP both use similar request structure (with differences in payment fields). 
+ * @deprecated For CP, refer examples in https://github.com/AuthorizeNet/sample-code-php/tree/master/PaymentTransactions
 */
 trigger_error('AuthorizeNetCP is deprecated, use AuthorizeNet::API instead. For CP, see examples in https://github.com/AuthorizeNet/sample-code-php/tree/master/PaymentTransactions .', E_USER_DEPRECATED);
 
