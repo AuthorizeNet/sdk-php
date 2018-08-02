6c28df45969a5490ffb4825c9e08993e8203bd7f
Kaushik <kikmak42@null.net>
2018-08-02 17:51:45 +0530

    Added Json Serailizer/de-serializers to the contracts

--- lib/net/authorize/api/contract/v1/SubMerchantType.php
+++ lib/net/authorize/api/contract/v1/SubMerchantType.php
@@ -8,7 +8,7 @@ namespace net\authorize\api\contract\v1;
  * 
  * XSD Type: subMerchantType
  */
-class SubMerchantType
+class SubMerchantType implements \JsonSerializable
 {
 
     /**
@@ -308,6 +308,95 @@ class SubMerchantType
         return $this;
     }
 
+
+    // Json Serialize Code
+    public function jsonSerialize(){
+        $values = array_filter((array)get_object_vars($this),
+        function ($val){
+            return !is_null($val);
+        });
+        $mapper = \net\authorize\util\Mapper::Instance();
+        // echo __CLASS__ . "\n";
+        foreach($values as $key => $value){
+            $classDetails = $mapper->getClass(get_class() , $key);
+            if (isset($value)){
+                //$classDetails = (new \net\authorize\api\contract\v1\Mapper)->getClass(get_class() , $key);
+                if ($classDetails->className === 'Date'){
+                    $dateTime = $value->format('Y-m-d');
+                    $values[$key] = $dateTime;
+                    //echo($dateTime."\n");
+                }
+                else if ($classDetails->className === 'DateTime'){
+                    $dateTime = $value->format('Y-m-d\TH:i:s\Z');
+                    $values[$key] = $dateTime;
+                    //echo($dateTime."\n");
+                }
+                if (is_array($value)){
+
+                    //echo "key - $key \n";
+                    //echo "value - $value \n";
+                    if (!$classDetails->isInlineArray){
+                        $subKey = $classDetails->arrayEntryname;
+                        $subArray = [$subKey => $value];
+                        $values[$key] = $subArray;
+                        //echo "subkey - $subKey \n";
+                    }
+                }
+            }
+        }
+        if (get_parent_class() == ""){
+            return $values;
+        }
+        else{
+            return array_merge(parent::jsonSerialize(), $values);
+        }
+    }
+
+    // Json Set Code
+    public function set($data)
+    {
+        $mapper = \net\authorize\util\Mapper::Instance();
+        foreach($data AS $key => $value) {
+            $classDetails = $mapper->getClass(get_class() , $key);
+
+            if($classDetails !== NULL ) {
+                if ($classDetails->isArray) {
+                    if ($classDetails->isCustomDefined) {
+                        foreach($value AS $keyChild => $valueChild) {
+                            $type = new $classDetails->className;
+                            $type->set($valueChild);
+                            $this->{'addTo' . $key}($type);
+                        }
+                    }
+                    else if ($classDetails->className === 'DateTime' || $classDetails->className === 'Date' ) {
+                        foreach($value AS $keyChild => $valueChild) {
+                            $type = new \DateTime($valueChild);
+                            $this->{'addTo' . $key}($type);
+                        }
+                    }
+                    else {
+                        foreach($value AS $keyChild => $valueChild) {
+                            $this->{'addTo' . $key}($valueChild);
+                        }
+                    }
+                }
+                else {
+                    if ($classDetails->isCustomDefined){
+                        $type = new $classDetails->className;
+                        $type->set($value);
+                        $this->{'set' . $key}($type);
+                    }
+                    else if ($classDetails->className === 'DateTime' || $classDetails->className === 'Date' ) {
+                        $type = new \DateTime($value);
+                        $this->{'set' . $key}($type);
+                    }
+                    else {
+                        $this->{'set' . $key}($value);
+                    }
+                }
+            }
+        }
+    }
 
 }
 
