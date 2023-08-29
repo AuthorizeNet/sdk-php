b08070ca603de76b7505c0cea7e09b279de79ce1
Alejo Moreira <alejo.moreira@729solutions.com>
2023-08-29 13:23:49 -0300

    PHP 8.2 compatibility error fixes

--- lib/net/authorize/api/contract/v1/AuthenticateTestRequest.php
+++ lib/net/authorize/api/contract/v1/AuthenticateTestRequest.php
@@ -7,28 +7,29 @@ namespace net\authorize\api\contract\v1;
  */
 class AuthenticateTestRequest extends ANetApiRequestType
 {
-
-
     // Json Serialize Code
-    public function jsonSerialize(){
-        $values = array_filter((array)get_object_vars($this),
-        function ($val){
-            return !is_null($val);
-        });
+    #[\ReturnTypeWillChange]
+    public function jsonSerialize()
+    {
+        $values = array_filter(
+            (array)get_object_vars($this),
+            function ($val) {
+                return !is_null($val);
+            }
+        );
         $mapper = \net\authorize\util\Mapper::Instance();
-        foreach($values as $key => $value){
-            $classDetails = $mapper->getClass(get_class() , $key);
-            if (isset($value)){
-                if ($classDetails->className === 'Date'){
+        foreach($values as $key => $value) {
+            $classDetails = $mapper->getClass(get_class(), $key);
+            if (isset($value)) {
+                if ($classDetails->className === 'Date') {
                     $dateTime = $value->format('Y-m-d');
                     $values[$key] = $dateTime;
-                }
-                else if ($classDetails->className === 'DateTime'){
+                } elseif ($classDetails->className === 'DateTime') {
                     $dateTime = $value->format('Y-m-d\TH:i:s\Z');
                     $values[$key] = $dateTime;
                 }
-                if (is_array($value)){
-                    if (!$classDetails->isInlineArray){
+                if (is_array($value)) {
+                    if (!$classDetails->isInlineArray) {
                         $subKey = $classDetails->arrayEntryname;
                         $subArray = [$subKey => $value];
                         $values[$key] = $subArray;
@@ -40,4 +41,3 @@ class AuthenticateTestRequest extends ANetApiRequestType
     }
 
 }
-
--- lib/net/authorize/api/contract/v1/CreateCustomerProfileTransactionRequest.php
+++ lib/net/authorize/api/contract/v1/CreateCustomerProfileTransactionRequest.php
@@ -7,7 +7,6 @@ namespace net\authorize\api\contract\v1;
  */
 class CreateCustomerProfileTransactionRequest extends ANetApiRequestType
 {
-
     /**
      * @property \net\authorize\api\contract\v1\ProfileTransactionType $transaction
      */
@@ -64,25 +63,28 @@ class CreateCustomerProfileTransactionRequest extends ANetApiRequestType
 
 
     // Json Serialize Code
-    public function jsonSerialize(){
-        $values = array_filter((array)get_object_vars($this),
-        function ($val){
-            return !is_null($val);
-        });
+    #[\ReturnTypeWillChange]
+    public function jsonSerialize()
+    {
+        $values = array_filter(
+            (array)get_object_vars($this),
+            function ($val) {
+                return !is_null($val);
+            }
+        );
         $mapper = \net\authorize\util\Mapper::Instance();
-        foreach($values as $key => $value){
-            $classDetails = $mapper->getClass(get_class() , $key);
-            if (isset($value)){
-                if ($classDetails->className === 'Date'){
+        foreach($values as $key => $value) {
+            $classDetails = $mapper->getClass(get_class(), $key);
+            if (isset($value)) {
+                if ($classDetails->className === 'Date') {
                     $dateTime = $value->format('Y-m-d');
                     $values[$key] = $dateTime;
-                }
-                else if ($classDetails->className === 'DateTime'){
+                } elseif ($classDetails->className === 'DateTime') {
                     $dateTime = $value->format('Y-m-d\TH:i:s\Z');
                     $values[$key] = $dateTime;
                 }
-                if (is_array($value)){
-                    if (!$classDetails->isInlineArray){
+                if (is_array($value)) {
+                    if (!$classDetails->isInlineArray) {
                         $subKey = $classDetails->arrayEntryname;
                         $subArray = [$subKey => $value];
                         $values[$key] = $subArray;
@@ -94,4 +96,3 @@ class CreateCustomerProfileTransactionRequest extends ANetApiRequestType
     }
 
 }
-
